fe1f96b5c6d4248dadbbee165da259a7eb4dde3b
media2net-app <chiel@187n.ai>
2026-09-10 16:04:24 +0300

    Apply 187N flagship identity across the Connections Hub

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,22 +4,13 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { App as AntApp, ConfigProvider } from 'antd'
 
-// Frydai fonts (self-hosted)
-import '@fontsource/plus-jakarta-sans/400.css'
-import '@fontsource/plus-jakarta-sans/500.css'
-import '@fontsource/plus-jakarta-sans/600.css'
-import '@fontsource/plus-jakarta-sans/700.css'
-import '@fontsource/plus-jakarta-sans/800.css'
-import '@fontsource/space-mono/400.css'
-import '@fontsource/space-mono/700.css'
-
 import App from './App'
 import ErrorBoundary from '@/components/ErrorBoundary'
 import { store } from '@/app/redux/store'
 import { useAppSelector } from '@/app/redux/hooks'
-import { darkTheme, lightTheme } from '@/theme/frydaiTheme'
+import { darkTheme, lightTheme } from '@/theme/hubTheme'
 import './index.css'
-import '@/styles/frydai.css'
+import '@/styles/hub.css'
 
 function ThemedApp() {
   const dark = useAppSelector((s) => s.ui.dark)
